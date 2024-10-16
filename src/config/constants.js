f562911d26f87ee8c3567758c8dada7cb270d572
maulikBuzz <maulik.buzz@gmail.com>
2024-10-16 16:51:42 +0530

    set html pdf

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,5 +1,5 @@
-const APP_URL = "http://192.168.29.170:5000";
-// const APP_URL = "https://estimate-ease-backend.onrender.com";
+// const APP_URL = "http://192.168.29.170:5000";
+const APP_URL = "https://estimate-ease-backend.onrender.com";
 
 export  {
     APP_URL,
